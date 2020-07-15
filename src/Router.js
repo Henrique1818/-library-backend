3897d3df158714ed10ac26e43c97982ea583869e
Henrique1818 <luiz.henrique01827@gmail.com>
2020-07-14 21:51:22 -0300

    update, function Update, All

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const UseLibrary = require('./controllers/UseLibrary');
 
 router.post('/', UseLibrary.create);
-
+router.put('/:id', UseLibrary.update);
+router.get('/', UseLibrary.all);
 
 
 module.exports = router;
--- src/controllers/UseLibrary.js
+++ src/controllers/UseLibrary.js
@@ -22,13 +22,57 @@ class UseLibrary {
             autores
         });
 
-        fs.writeFile("src/model/Data.json", JSON.stringify(Data, null, 2), (err) => {
+        fs.writeFile('src/model/Data.json', JSON.stringify(Data, null, 2), (err) => {
             if(err) return res.status(500).json({err: 'Write file error'})
         })
 
         return res.status(200).json({books: Data.books})
     }
 
+    update(req, res) {
+        const { id } = req.params
+
+        let index = 0;
+
+        const foundBook = Data.books.find((book, foundIndex) => {
+            if(id == book.id) {
+                index = foundIndex
+                return true
+            }
+        })
+
+        if(!foundBook) return res.status(404).json({error: 'Not found Book'});
+
+        if(!req.body.title) return res.status(400).json({error: 'titulo é obrigatorio'})
+        if(!req.body.editora) return res.status(400).json({error: 'editora é obrigatorio'})
+        if(!req.body.foto) return res.status(400).json({error: 'foto é obrigatorio'})
+        if(!req.body.autores) return res.status(400).json({error: 'autores é obrigatorio'})
+
+        const book = {
+            ...foundBook,
+            ...req.body
+        }
+
+        Data.books[index] = book
+
+        fs.writeFile('src/model/Data.json', JSON.stringify(Data, null, 2), (err) => {
+            if(err) return res.status(500).json(err)
+
+            return res.status(200).json({ book })
+        } )
+    }
+
+    all(req, res) {
+        const book = Data.books.map(book => {
+            const newBook = {
+                ...book
+            }
+
+            return newBook
+        })
+        
+        return res.status(200).json({ book })
+    }
 }
 
 module.exports = new UseLibrary();
