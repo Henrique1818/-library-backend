ae6e76e30a27a2e8389591e2c0e34acc0e95f874
Henrique1818 <luiz.henrique01827@gmail.com>
2020-07-21 23:12:21 -0300

    Tentando seguir os padrões REST, Testes do Insomnia

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const LibraryController = require('./controllers/LibraryController');
 const LibraryValidation = require('./middlewares/LibraryValidation');
 
-router.get('/construction', LibraryController.all);
-router.get('/construction/library', LibraryController.filterCategory);
+router.get('/library', LibraryController.all);
+router.get('/library/book', LibraryController.filterCategory);
 
-router.post('/construction', LibraryValidation, LibraryController.create);
-router.put('/construction/:id', LibraryValidation, LibraryController.update);
-router.get('/construction/:id', LibraryController.show);
-router.delete('/construction/:id', LibraryController.delete);
+router.post('/library', LibraryValidation, LibraryController.create);
+router.put('/library/:id', LibraryValidation, LibraryController.update);
+router.get('/library/:id', LibraryController.show);
+router.delete('/library/:id', LibraryController.delete);
 
 module.exports = router;
